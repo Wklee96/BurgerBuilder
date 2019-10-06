195078b24302b37480d1db2207ae68ab17545f33
Wklee96 <lweikang96@gmail.com>
2019-10-06 16:20:24 +0800

    added navigation items

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Auxiliary from "../../hoc/Auxiliary";
 import classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
-const layout = (props) => {
-    return (
-        <Auxiliary>
-            <div>Toolbar, SideDrawer, Backdrop</div>
-            <main className={classes.Content }>
-                {props.children}
-            </main>
-        </Auxiliary>
-    )
-}
+const layout = props => {
+  return (
+    <Auxiliary>
+      <Toolbar />
+      <main className={classes.Content}>{props.children}</main>
+    </Auxiliary>
+  );
+};
 
 export default layout;
