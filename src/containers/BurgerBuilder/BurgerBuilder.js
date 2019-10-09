1d7b61b6ff774e62f7a4b0c8a2b1bc8a5f03b03d
Wklee96 <lweikang96@gmail.com>
2019-10-09 15:05:02 +0800

    added router and contact data

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -45,35 +45,46 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "WK",
-        address: {
-          street: "HEY",
-          zip: "123123"
-        },
-        email: "test@test.com"
-      }
-    };
-    axios
-      .post("/orders.json", order)
-      .then(response => {
-        this.setState({
-          purchasing: false,
-          loading: false
-        });
-        console.log(response);
-      })
-      .catch(error => {
-        this.setState({
-          purchasing: false,
-          loading: false
-        });
-        console.log(error);
-      });
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: "WK",
+    //     address: {
+    //       street: "HEY",
+    //       zip: "123123"
+    //     },
+    //     email: "test@test.com"
+    //   }
+    // };
+    // axios
+    //   .post("/orders.json", order)
+    //   .then(response => {
+    //     this.setState({
+    //       purchasing: false,
+    //       loading: false
+    //     });
+    //     console.log(response);
+    //   })
+    //   .catch(error => {
+    //     this.setState({
+    //       purchasing: false,
+    //       loading: false
+    //     });
+    //     console.log(error);
+    //   });
+
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + "=" + encodeURIComponent(this.state.ingredients[i]));
+    }
+    const queryString = queryParams.join("&");
+
+    this.props.history.push({
+      pathname: "/checkout",
+      search: "?" + queryString,      
+    });
   };
 
   updatePurchaseState(ingredients) {
